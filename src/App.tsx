798a283f4127673271b656cfa6a141dcb8c1871a
KarlaMacedo <karlapaolamacedo@gmail.com>
2024-10-31 19:07:10 -0600

    Update Fix Background color

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import "./App.css"
 export const App: React.FC = () => {
   return (
     <Router>
+      {/* Fondo radial */}
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
